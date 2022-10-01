c8ec2166039913f20eb27182a83104d33ff5de36
andrew.lawendy <andrew.lawendy@tempo.fit>
2022-10-01 13:03:59 +0200

    style(chakraui): :lipstick: add chakra ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,11 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { ChakraProvider } from "@chakra-ui/react";
+import { Heading } from "@chakra-ui/react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ChakraProvider>
+      <Heading>App working</Heading>
+    </ChakraProvider>
   );
 }
 
