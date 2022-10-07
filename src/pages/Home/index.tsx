99b9b99d712b07daa5b724c63cf94c69ea473958
andrew.lawendy <andrew.lawendy@tempo.fit>
2022-10-07 12:44:54 +0200

    feat(fetch next outfit): :sparkles: add new button

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,7 +1,26 @@
-import { Heading } from "@chakra-ui/react";
+import { Heading, Button } from "@chakra-ui/react";
 
 const Home = () => {
-  return <Heading>Home</Heading>;
+  return (
+    <>
+      <Heading>Home</Heading>
+
+      <Button
+        size="lg"
+        sx={{
+          backgroundColor: "teal.400",
+          color: "white",
+          borderRadius: "3xl",
+          position: "fixed",
+          bottom: 6,
+          left: "50%",
+          transform: "translate3d(-50%, 0, 0)",
+        }}
+      >
+        Fetch Next Outfit
+      </Button>
+    </>
+  );
 };
 
 export default Home;
