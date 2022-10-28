4a912d16cde63b2a4eadaaac23bdb3652675ffdb
andrew.lawendy <andrew.lawendy@tempo.fit>
2022-10-29 00:04:57 +0200

    feat(useData): :sparkles: include document id in the object

--- src/resources/useData.ts
+++ src/resources/useData.ts
@@ -1,24 +1,46 @@
+import { useMemo } from "react";
 import {
   collection,
   query,
   where,
   QueryConstraint,
-  CollectionReference,
+  PartialWithFieldValue,
+  DocumentData,
+  QueryDocumentSnapshot,
+  SnapshotOptions,
 } from "firebase/firestore";
 import { useCollectionData } from "react-firebase-hooks/firestore";
 
 import useAuth from "hooks/useAuth";
 import { db } from "utils/firebase";
 
-const useData = <T>(
+const useData = <T extends DocumentData>(
   collectionName: string,
   ...queryConstraints: QueryConstraint[]
 ) => {
   const [user] = useAuth();
-  const q = query(
-    collection(db, collectionName) as CollectionReference<T>,
-    where("user", "==", user?.uid),
-    ...queryConstraints
+  const q = useMemo(
+    () =>
+      query(
+        collection(db, collectionName).withConverter({
+          toFirestore({ id, ...data }: PartialWithFieldValue<T>): DocumentData {
+            return data;
+          },
+          fromFirestore(
+            snapshot: QueryDocumentSnapshot<T>,
+            options: SnapshotOptions
+          ): T {
+            const data = snapshot.data(options);
+            return {
+              id: snapshot.id,
+              ...data,
+            };
+          },
+        }),
+        where("user", "==", user?.uid),
+        ...queryConstraints
+      ),
+    []
   );
   return useCollectionData(q, {
     snapshotListenOptions: { includeMetadataChanges: true },
