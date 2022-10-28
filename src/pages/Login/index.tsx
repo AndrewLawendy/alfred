3d5f77ecfaed44cb1103e3e8786ab30700fdac0d
andrew.lawendy <andrew.lawendy@tempo.fit>
2022-10-28 14:35:55 +0200

    feat(login): :sparkles: add login page with its logic

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -5,16 +5,50 @@ import {
   FacebookAuthProvider,
   signInWithRedirect,
 } from "firebase/auth";
-import { Button } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  Image,
+  Heading,
+  Text,
+  Icon,
+  Spinner,
+} from "@chakra-ui/react";
+import { FaFacebookSquare, FaGoogle } from "react-icons/fa";
+import { motion } from "framer-motion";
 
 import useAuth from "hooks/useAuth";
 import { auth } from "utils/firebase";
 
+import Logo from "assets/logo.png";
+
 const googleAuthProvider = new GoogleAuthProvider();
 const facebookAuthProvider = new FacebookAuthProvider();
 
+const container = {
+  hidden: { opacity: 0, y: -10 },
+  show: {
+    opacity: 1,
+    y: 0,
+    transition: {
+      staggerChildren: 0.2,
+    },
+  },
+};
+
+const item = {
+  hidden: { opacity: 0, y: -10 },
+  show: {
+    opacity: 1,
+    y: 0,
+    transition: {
+      duration: 0.5,
+    },
+  },
+};
+
 const Login = () => {
-  const [user] = useAuth();
+  const [user, isLoading] = useAuth();
   const [, setLocation] = useLocation();
 
   useEffect(() => {
@@ -24,20 +58,84 @@ const Login = () => {
   }, [user]);
 
   return (
-    <>
-      <Button
-        colorScheme="facebook"
-        onClick={() => signInWithRedirect(auth, facebookAuthProvider)}
-      >
-        Login with Facebook
-      </Button>
-      <Button
-        colorScheme="red"
-        onClick={() => signInWithRedirect(auth, googleAuthProvider)}
-      >
-        Login with Google
-      </Button>
-    </>
+    <Box
+      as={motion.div}
+      sx={{ py: 20, px: 3, textAlign: "center" }}
+      variants={container}
+      initial="hidden"
+      animate="show"
+    >
+      <motion.div variants={item}>
+        <Image src={Logo} sx={{ maxH: 28, mx: "auto" }} />
+      </motion.div>
+      <motion.div variants={item}>
+        <Heading
+          sx={{
+            mt: 4,
+            fontFamily: "advent",
+          }}
+        >
+          Alfred
+        </Heading>
+      </motion.div>
+      <motion.div variants={item}>
+        <Text fontSize="xl">Your own wardrobe stylist</Text>
+      </motion.div>
+
+      <Box sx={{ mt: 32 }}>
+        <motion.div variants={item}>
+          <Text sx={{ fontSize: "lg", fontWeight: "semibold", mb: 4 }}>
+            Login or create an account
+          </Text>
+        </motion.div>
+        <motion.div variants={item}>
+          <Button
+            sx={{ width: "100%" }}
+            size="lg"
+            colorScheme="facebook"
+            onClick={() => signInWithRedirect(auth, facebookAuthProvider)}
+            leftIcon={<Icon as={FaFacebookSquare} />}
+          >
+            Continue with Facebook
+          </Button>
+        </motion.div>
+        <motion.div variants={item}>
+          <Button
+            sx={{ width: "100%", mt: 4 }}
+            size="lg"
+            colorScheme="red"
+            onClick={() => signInWithRedirect(auth, googleAuthProvider)}
+            leftIcon={<Icon as={FaGoogle} />}
+          >
+            Continue with Google
+          </Button>
+        </motion.div>
+      </Box>
+
+      {isLoading && (
+        <Box
+          sx={{
+            position: "fixed",
+            top: 0,
+            left: 0,
+            width: "100%",
+            height: "100%",
+            backgroundColor: "whiteAlpha.800",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <Spinner
+            thickness="4px"
+            speed="0.65s"
+            emptyColor="gray.200"
+            color="teal.500"
+            size="xl"
+          />
+        </Box>
+      )}
+    </Box>
   );
 };
 
