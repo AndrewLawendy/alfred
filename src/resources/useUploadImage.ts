2ed2ecaf5e2dc611d7fced49bd53dd8fd3f96fbc
andrew.lawendy <andrew.lawendy@tempo.fit>
2022-10-29 19:06:12 +0200

    feat(upload image): :sparkles: support replace

--- src/resources/useUploadImage.ts
+++ src/resources/useUploadImage.ts
@@ -10,14 +10,15 @@ import { useUploadFile } from "react-firebase-hooks/storage";
 import { storage } from "utils/firebase";
 
 const useUploadImage = (): [
-  (file: File) => Promise<UploadResult | undefined>,
+  (file: File, imageUrl?: string) => Promise<UploadResult | undefined>,
   boolean,
   UploadTaskSnapshot | undefined,
   StorageError | undefined
 ] => {
   const [uploadFile, ...rest] = useUploadFile();
-  const uploadImage = (file: File) => {
-    const storageRef = ref(storage, `${Date.now()}-${file.name}`);
+  const uploadImage = (file: File, imageUrl?: string) => {
+    const path = imageUrl ?? `${Date.now()}-${file.name}`;
+    const storageRef = ref(storage, path);
     return uploadFile(storageRef, file, {
       contentType: "image/jpeg",
     });
